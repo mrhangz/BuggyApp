0d18e80962fdb7b3e5ad39439e737f9995fd2e28
Teerawat Vanasapdamrong <teerawat.vanasapdamrong@scb.co.th>
2019-06-28 11:28:28 +0700

    Initial Commit

---
+++ BuggyApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BuggyApp
+//
+//  Created by Teerawat Vanasapdamrong on 28/6/19.
+//  Copyright © 2019 scbeasy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
