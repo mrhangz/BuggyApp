f4510f6cc86e38f3865570296f90c20444c6b532
sakarn.limnitikarn <sakarn.limnitikarn@scb.co.th>
2019-07-04 16:58:46 +0700

    update layout, add image assets

--- BuggyApp/Model.swift
+++ BuggyApp/Model.swift
@@ -44,68 +44,7 @@ struct Track {
   let country: String
   let currency: String
   let primaryGenreName: String
-
-////  enum CodingKeys: String, CodingKey {
-////    case artistId
-////    case artistName
-////    case collectionName
-////    case trackName
-////    case artworkUrl = "artworkUrl100"
-////    case collectionPrice
-////    case trackPrice
-////    case releaseDate
-////    case trackTimeMillis
-////    case country
-////    case currency
-////    case primaryGenreName
-////  }
-//
-//  init(from decoder: Decoder) throws {
-//    let values = try decoder.container(keyedBy: CodingKeys.self)
-//    artistId = try values.decode(Int.self, forKey: .artistId)
-//    artistName = try values.decode(String.self, forKey: .artistName)
-//    collectionName = try values.decode(String.self, forKey: .collectionName)
-//    trackName = try values.decode(String.self, forKey: .trackName)
-//    artworkUrl = try values.decode(String.self, forKey: .artworkUrl)
-//    collectionPrice = try values.decode(Double.self, forKey: .collectionPrice)
-//    trackPrice = try values.decode(Double.self, forKey: .trackPrice)
-//    releaseDate = try values.decode(String.self, forKey: .releaseDate)
-//    trackTimeMillis = try values.decode(Int.self, forKey: .trackTimeMillis)
-//    country = try values.decode(String.self, forKey: .country)
-//    currency = try values.decode(String.self, forKey: .currency)
-//    primaryGenreName = try values.decode(String.self, forKey: .primaryGenreName)
-//  }
 }
-
-//struct Track: Codable {
-//  let wrapperType, kind: String
-//  let artistID, collectionID, trackID: Int
-//  let artistName, collectionName, trackName, collectionCensoredName: String
-//  let trackCensoredName: String
-//  let artistViewURL, collectionViewURL, trackViewURL: String
-//  let previewURL: String
-//  let artworkUrl30, artworkUrl60, artworkUrl100: String
-//  let collectionPrice, trackPrice: Double
-//  let releaseDate: String
-//  let collectionExplicitness, trackExplicitness: String
-//  let discCount, discNumber, trackCount, trackNumber: Int
-//  let trackTimeMillis: Int
-//  let country, currency, primaryGenreName: String
-//  let isStreamable: Bool
-//
-//  enum CodingKeys: String, CodingKey {
-//    case wrapperType, kind
-//    case artistID = "artistId"
-//    case collectionID = "collectionId"
-//    case trackID = "trackId"
-//    case artistName, collectionName, trackName, collectionCensoredName, trackCensoredName
-//    case artistViewURL = "artistViewUrl"
-//    case collectionViewURL = "collectionViewUrl"
-//    case trackViewURL = "trackViewUrl"
-//    case previewURL = "previewUrl"
-//    case artworkUrl30, artworkUrl60, artworkUrl100, collectionPrice, trackPrice, releaseDate, collectionExplicitness, trackExplicitness, discCount, discNumber, trackCount, trackNumber, trackTimeMillis, country, currency, primaryGenreName, isStreamable
-//  }
-//}
 
 extension Track {
   init(json: JSON) {
